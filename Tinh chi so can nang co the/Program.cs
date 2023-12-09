906351f9535d0216dbde89746af8fa854a902703
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-09 17:14:14 +0700

    Da xong

---
+++ Tinh chi so can nang co the/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace baitap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chieuCao;
+            double canNang;
+            Console.WriteLine("Nhap chieu cao:");
+            chieuCao = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap can nang cua ban:");
+            canNang = float.Parse(Console.ReadLine());
+            double bmi = canNang / Math.Pow(chieuCao, 2);
+            bmi = Math.Round(bmi, 1);
+            Console.WriteLine("Chi so BMI cua ban la:" + bmi);
+            if (bmi < 18)
+                Console.WriteLine("Thieu can");
+            else if (bmi < 25.0)
+                Console.WriteLine("Binh thuong");
+            else if (bmi < 30.0)
+                Console.WriteLine("Thua can");
+            else
+                Console.WriteLine("Beo phi");
+            Console.ReadKey();
+        }
+    }
+}
